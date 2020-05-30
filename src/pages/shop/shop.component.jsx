42f881f56602a07a2d303081e53bae2000502c03
Rajesh Rana <rajesh.rana@zohomail.com>
2020-05-30 23:14:03 +0530

    created directory and shop reducer for collection overview and collection items

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import CollectionOverview from '../../components/collecton-overview/collection-overview.component';
+import Collection from '../collection/collection.component';
 
-import SHOP_DATA from './shop.data.js';
-
-import CollectionPreview from '../../components/collection-preview/collecion-preview.compenent';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={Collection} />
+  </div>
+);
 
 export default ShopPage;
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+const selectShop = (state) => state.shop;
+
+export const selectShopCollections = createSelector([selectShop], (shop) => {
+  return shop.collections;
+});
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = (collectionUrlParams) =>
+  createSelector(
+    [selectShopCollections],
+    (collections) => collections[collectionUrlParams]
+  );
