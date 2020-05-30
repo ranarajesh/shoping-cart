f6baa8e24494894bdeb081437bbab7ba1a49b7ee
Rajesh Rana <rajesh.rana@zohomail.com>
2020-05-30 18:15:42 +0530

    added redux persistorgate component

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,4 +1,7 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
@@ -7,4 +10,10 @@ const rootReducer = combineReducers({
   cart: cartReducer,
 });
 
-export default rootReducer;
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+export default persistReducer(persistConfig, rootReducer);
